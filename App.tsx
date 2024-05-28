d5c9ade2e8df2d5bf4d430857a1cf5f74140fb10
Serban Petrescu <Serban.Petrescu@outlook.com>
2024-05-28 06:37:23 +0000

    feat(boilerplate): Fixes some assets, adds SVG support.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Activity from './assets/activity.svg';
+
 export default function App() {
     return (
         <View style={styles.container}>
             <Text>Open up App.tsx to start working on your app!</Text>
+            <Text>Example of using a SVG in the app:</Text>
+            <Activity color="rgb(255, 0, 0)" width={24} height={24} />
             <StatusBar style="auto" />
         </View>
     );
